f45e2e4ca7d640c02924d389cf60dbb59e40a0a3
Abhilasha kumari <abhilashakumari455@gmail.com>
2025-08-07 14:09:19 +0530

    Disabled test again

--- src/test/java/com/edigest/journalApp/service/UserRepoImplTest.java
+++ src/test/java/com/edigest/journalApp/service/UserRepoImplTest.java
@@ -1,26 +1,21 @@
 package com.edigest.journalApp.service;
 
-
 import com.edigest.journalApp.entity.UserRepoImpl;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@Disabled("Temporarily disabled due to MongoDB connection issue") // <--- Disable entire class
 @SpringBootTest
 public class UserRepoImplTest {
-
 
     @Autowired
     private UserRepoImpl userRepoImpl;
 
-    @Disabled("Temporarily disabled due to MongoDB connection issue")
     @Test
-    public void testSaveUser(){
-
+    public void testSaveUser() {
         userRepoImpl.getUserForSA();
-
     }
 
 }
